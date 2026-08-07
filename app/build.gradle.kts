407ee4da2ae64d28514383bebacc2d3f0bd673a8
JuanchR4 <juanluisdeleoncardona30@gmail.com>
2026-08-06 21:50:58 -0500

    Prepare controls build for clean installation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
-android { namespace="com.dualrobotics.maxcontrol"; compileSdk=35; compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }; defaultConfig { applicationId="com.dualrobotics.maxcontrol.v2"; minSdk=26; targetSdk=35; versionCode=1; versionName="1.0.0" } }
+android { namespace="com.dualrobotics.maxcontrol"; compileSdk=35; compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }; defaultConfig { applicationId="com.dualrobotics.maxcontrol.v3"; minSdk=26; targetSdk=35; versionCode=1; versionName="1.0.0" } }
 dependencies { implementation("androidx.core:core-ktx:1.13.1"); implementation("androidx.activity:activity-ktx:1.9.2"); implementation("com.google.android.material:material:1.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0") }
 
